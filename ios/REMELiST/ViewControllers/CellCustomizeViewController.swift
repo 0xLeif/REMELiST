c02e61eebfd2805e7dd01b4bce6e1a808ee45651
zmeriksen <zmeriksen@icloud.com>
2020-05-11 21:04:02 -0500

    Create backgroundColor selector

--- ios/REMELiST/ViewControllers/CellCustomizeViewController.swift
+++ ios/REMELiST/ViewControllers/CellCustomizeViewController.swift
@@ -12,33 +12,134 @@ import SwiftUIKit
 class CellCustomizeViewController: UIViewController {
     private lazy var settingsTable = Table {
         [
-            Label.headline("Margin"),
-            Slider(value: globalStyle.margin, from: 0, to: 32) { [weak self] value in
-                globalStyle.margin = value
-                self?.styledView.apply(style: globalStyle)
-            }
-            .padding(4),
+            VStack {
+                [
+                    Label.headline("Margin"),
+                    Slider(value: globalStyle.margin, from: 0, to: 32) { [weak self] value in
+                        globalStyle.margin = value
+                        self?.styledView.apply(style: globalStyle)
+                    }
+                    .padding(4)
+                ]
+                
+            },
             
-            Label.headline("Padding"),
-            Slider(value: globalStyle.padding, from: 0, to: 32) { [weak self] value in
-                globalStyle.padding = value
-                self?.styledView.apply(style: globalStyle)
-            }
-            .padding(4),
+            VStack {
+                [
+                    Label.headline("Padding"),
+                    Slider(value: globalStyle.padding, from: 0, to: 32) { [weak self] value in
+                        globalStyle.padding = value
+                        self?.styledView.apply(style: globalStyle)
+                    }
+                    .padding(4)
+                ]
+            },
             
-            Label.headline("Border width"),
-            Slider(value: globalStyle.borderWidth, from: 0, to: 12) { [weak self] value in
-                globalStyle.borderWidth = value
-                self?.styledView.apply(style: globalStyle)
-            }
-            .padding(4),
+            VStack {
+                [
+                    Label.headline("Border width"),
+                    Slider(value: globalStyle.borderWidth, from: 0, to: 12) { [weak self] value in
+                        globalStyle.borderWidth = value
+                        self?.styledView.apply(style: globalStyle)
+                    }
+                    .padding(4)
+                ]
+            },
             
-            Label.headline("Corner Radius"),
-            Slider(value: globalStyle.cornerRadius, from: 0, to: 16) { [weak self] value in
-                globalStyle.cornerRadius = value
-                self?.styledView.apply(style: globalStyle)
+            VStack {
+                [
+                    Label.headline("Corner Radius"),
+                    Slider(value: globalStyle.cornerRadius, from: 0, to: 16) { [weak self] value in
+                        globalStyle.cornerRadius = value
+                        self?.styledView.apply(style: globalStyle)
+                    }
+                    .padding(4)
+                ]
+            },
+            
+            VStack {
+                var currentBackgroundColor = UIView()
+                
+                return [
+                    Label.headline("Background Color"),
+                    NavButton(destination: UIViewController {
+                        var view = UIView().background(color: globalStyle.backgroundColor)
+                        return view.embed {
+                            SafeAreaView {
+                                VStack {
+                                    [
+                                        UIView(backgroundColor: .white) {
+                                            VStack {
+                                                [
+                                                    Label("Red"),
+                                                    Slider(value: 255, from: 0, to: 255) { [weak self] (red) in
+                                                        globalStyle.backgroundColor = UIColor(red: CGFloat(red / 255),
+                                                                                              green: globalStyle.backgroundColor.green,
+                                                                                              blue: globalStyle.backgroundColor.blue,
+                                                                                              alpha: globalStyle.backgroundColor.alpha)
+                                                        self?.styledView.apply(style: globalStyle)
+                                                        view.background(color: globalStyle.backgroundColor)
+                                                        currentBackgroundColor.background(color: globalStyle.backgroundColor)
+                                                    },
+                                                    Label("Green"),
+                                                    Slider(value: 255, from: 0, to: 255) { [weak self] (green) in
+                                                        globalStyle.backgroundColor = UIColor(red: globalStyle.backgroundColor.red,
+                                                                                              green: CGFloat(green / 255),
+                                                                                              blue: globalStyle.backgroundColor.blue,
+                                                                                              alpha: globalStyle.backgroundColor.alpha)
+                                                        self?.styledView.apply(style: globalStyle)
+                                                        view.background(color: globalStyle.backgroundColor)
+                                                        currentBackgroundColor.background(color: globalStyle.backgroundColor)
+                                                    },
+                                                    Label("Blue"),
+                                                    Slider(value: 255, from: 0, to: 255) { [weak self] (blue) in
+                                                        globalStyle.backgroundColor = UIColor(red: globalStyle.backgroundColor.red,
+                                                                                              green: globalStyle.backgroundColor.green,
+                                                                                              blue: CGFloat(blue / 255),
+                                                                                              alpha: globalStyle.backgroundColor.alpha)
+                                                        self?.styledView.apply(style: globalStyle)
+                                                        view.background(color: globalStyle.backgroundColor)
+                                                        currentBackgroundColor.background(color: globalStyle.backgroundColor)
+                                                    },
+                                                    Label("Alpha"),
+                                                    Slider(value: 255, from: 0, to: 255) { [weak self] (alpha) in
+                                                        globalStyle.backgroundColor = UIColor(red: globalStyle.backgroundColor.red,
+                                                                                              green: globalStyle.backgroundColor.green,
+                                                                                              blue: globalStyle.backgroundColor.blue,
+                                                                                              alpha: CGFloat(alpha / 255))
+                                                        self?.styledView.apply(style: globalStyle)
+                                                        view.background(color: globalStyle.backgroundColor)
+                                                        currentBackgroundColor.background(color: globalStyle.backgroundColor)
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                        .padding()
+                                        .layer(cornerRadius: 8),
+                                        Spacer().background(color: .clear)
+                                    ]
+                                }
+                            }
+                        }
+                    }, style: .push) {
+                        HStack {
+                            [
+                                Label("Set Color"),
+                                Spacer(),
+                                currentBackgroundColor
+                                    .frame(width: 44)
+                                    .layer(borderWidth: 1)
+                                    .layer(borderColor: .black)
+                                    .layer(cornerRadius: 8)
+                                    .background(color: globalStyle.backgroundColor)
+                            ]
+                        }
+                        .frame(height: 44)
+                        .padding(4)
+                        
+                    }
+                ]
             }
-            .padding(4)
         ]
     }
     private var styledView = StyleView(style: globalStyle) {
